c196fba3c2660d5928664c22b0da248a20be27b0
Richard Pastenes <richard@pastenes.com>
2020-05-20 23:22:32 -0700

    Video 16

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,8 @@
-import Link from 'next/link';
-
-const Home = props => (
+import Items from '../components/Items'
+const Home = (props) => (
   <div>
-    <p>Hey!</p>
+    <Items />
   </div>
-);
+)
 
-export default Home;
+export default Home
